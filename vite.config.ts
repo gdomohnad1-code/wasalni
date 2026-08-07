77bc7adc4c6df10ff951f5a27af6ee4496ea4b9e
gdomohnad1-code <gdomohnad1@gmail.com>
2026-08-07 04:39:00 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -43,8 +43,7 @@ export default defineConfig({
           ],
         },
         workbox: {
-          globStrict: false,
-          globPatterns: ["**/*.{js,css,html}", "**/*.{svg,png,ico,jpg,jpeg,webp,woff,woff2}"],
+          globPatterns: ['**/*.{js,css,html,ico,png}'],
           globIgnores: ["**/node_modules/**/*", "sw.js", "workbox-*.js"],
           
           navigateFallback: "/offline.html",
