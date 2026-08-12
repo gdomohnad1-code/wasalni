30171906904ebb1a218f6f81aa4f7ed0567d80f3
gdomohnad1-code <gdomohnad1@gmail.com>
2026-08-12 16:28:27 +0300

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -1,16 +1,12 @@
 import { QueryClient } from "@tanstack/react-query";
-import { createRouter, createHashHistory } from "@tanstack/react-router";
+import { createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
-
-// Create a hash history instance for offline Capacitor routing
-const hashHistory = createHashHistory();
 
 export const getRouter = () => {
   const queryClient = new QueryClient();
 
   const router = createRouter({
     routeTree,
-    history: hashHistory, // <--- Inject the hash history here
     context: { queryClient },
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
